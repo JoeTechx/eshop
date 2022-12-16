797accdcf8cfba8a7397021ecbd2024622dd44b4
JOEX <oyinjoe23@gmail.com>
2022-12-16 15:44:41 +0100

    feature:folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import "./App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+//pages
+import { Home, Contact } from "./pages"
+//components
+import { Header, Footer } from "./components"
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!!!</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
