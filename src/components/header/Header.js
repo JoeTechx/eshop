762df116630c59f1f3e9ba93b2b7b89852b8c57c
JOEX <oyinjoe23@gmail.com>
2022-12-17 21:01:04 +0100

    feature: Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaShoppingCart } from 'react-icons/fa'
 import styles from "./Header.module.scss";
 
 const logo = (
@@ -12,10 +13,47 @@ const logo = (
   </div>
 );
 
+const cart = (
+  <span className={styles.cart}>
+  <Link to="/cart">
+    Cart
+    <FaShoppingCart size={20}/>
+    <p>0</p>
+    </Link>
+</span>
+)
+
 const Header = () => {
   return (
     <header>
-      <div className={styles.header}>{logo}</div>
+      <div className={styles.header}>
+        {logo}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">
+                Home
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/contact">
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+<div className={styles["header-right"]}>
+  <span className={styles.links}>
+    <Link to="/login">Login</Link>
+    <Link to="/register">Register</Link>
+    <Link to="/order-history">My Orders</Link>
+  </span>
+
+{cart}
+</div>
+
+        </nav>
+      </div>
     </header>
   );
 };
