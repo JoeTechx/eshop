4cfc45b2bc97076c52417f11c2464a6d3f461a8b
JOEX <oyinjoe23@gmail.com>
2022-12-18 03:19:43 +0100

    add: activeLink

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink} from "react-router-dom";
 import { FaShoppingCart, FaTimes } from "react-icons/fa";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import styles from "./Header.module.scss";
@@ -14,61 +14,77 @@ const logo = (
   </div>
 );
 
+const activeLink =(
+  ({isActive}) => 
+  (isActive ? `${styles.active}` : "")
+)
+
 const cart = (
   <span className={styles.cart}>
-    <Link to="/cart">
+    <NavLink to="/cart" className={activeLink}>
       Cart
       <FaShoppingCart size={20} />
       <p>0</p>
-    </Link>
+    </NavLink>
   </span>
 );
 
 const Header = () => {
-  const [showMenu, setShowMenu] = useState(false)
+  const [showMenu, setShowMenu] = useState(false);
 
-  const toggleMenu = () =>{
-    setShowMenu(!showMenu)
-  }
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
-  const hideMenu = () =>{
-    setShowMenu(false)
-  }
+  const hideMenu = () => {
+    setShowMenu(false);
+  };
 
   return (
     <header>
       <div className={styles.header}>
         {logo}
-        <nav className={showMenu ? `${styles["show-nav"]}` : `${styles["hide-nav"]}`}>
-
-          <div className={showMenu ? `${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}` : `${styles["nav-wrapper"]}`} onClick={hideMenu}></div>
+        <nav
+          className={
+            showMenu ? `${styles["show-nav"]}` : `${styles["hide-nav"]}`
+          }
+        >
+          <div
+            className={
+              showMenu
+                ? `${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}`
+                : `${styles["nav-wrapper"]}`
+            }
+            onClick={hideMenu}
+          ></div>
           <ul onClick={hideMenu}>
-            <li className={styles['logo-mobile']}>
-            {logo}
-            <FaTimes size={22} color="#fff" onClick={hideMenu}/>
-            </li>
-            <li>
-              <Link to="/">Home</Link>
+
+            <li className={styles["logo-mobile"]}>
+              {logo}
+              <FaTimes size={22} color="#fff" onClick={hideMenu} />
             </li>
 
             <li>
-              <Link to="/contact">Contact Us</Link>
+              <NavLink to="/" className={activeLink}>Home</NavLink>
+            </li>
+
+            <li>
+              <NavLink to="/contact" className={activeLink}>Contact Us</NavLink>
             </li>
           </ul>
           <div className={styles["header-right"]} onClick={hideMenu}>
             <span className={styles.links}>
-              <Link to="/login">Login</Link>
-              <Link to="/register">Register</Link>
-              <Link to="/order-history">My Orders</Link>
+              <NavLink to="/login" className={activeLink}>Login</NavLink>
+              <NavLink to="/register" className={activeLink}>Register</NavLink>
+              <NavLink to="/order-history" className={activeLink}>My Orders</NavLink>
             </span>
             {cart}
           </div>
-          
         </nav>
 
         <div className={styles["menu-icon"]}>
-        {cart}
-        <HiOutlineMenuAlt3 size={28} onClick={toggleMenu}/>
+          {cart}
+          <HiOutlineMenuAlt3 size={28} onClick={toggleMenu} />
         </div>
       </div>
     </header>
