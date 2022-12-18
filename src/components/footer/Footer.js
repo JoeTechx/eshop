c27dd8c7097ef94a3ba82676552c71e98379a48f
JOEX <oyinjoe23@gmail.com>
2022-12-18 03:58:04 +0100

    feat: Login, Register, Reset

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from "./Footer.module.scss"
 
-const date = new Date;
+const date = new Date();
 const Year = date.getFullYear();
 const Footer = () => {
   return (
