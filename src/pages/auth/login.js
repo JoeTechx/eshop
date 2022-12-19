81d6d7e389d83ff65b514c6ab2f28a313dff46f7
JOEX <oyinjoe23@gmail.com>
2022-12-19 23:41:00 +0100

    feat: Login

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,7 +1,47 @@
 import React from "react";
+import styles from './auth.module.scss'
+import LoginImg from "../../assets/login.png"
+import { Link } from "react-router-dom";
+import { FaGoogle } from  "react-icons/fa"
+import Card from "../../components/card/Card";
+
+
 
 const login = () => {
-  return <div>login</div>;
+  return(
+    <section className={ ` container ${styles.auth}`}>
+    <div className={styles.img}>
+        <img src={LoginImg} alt='Login' width="400"/>
+      </div>
+
+<Card>
+      <div className={styles.form}>
+          <h2>Login</h2>
+          <form>
+            <input type='email' placeholder="Email" required/>
+            <input type='password' placeholder="Password" required/>
+
+            <button className="--btn --btn-primary --btn-block">Login</button>
+
+            <div className={styles.links}>
+              <Link to="/reset">Forget Password</Link>
+            </div>
+            <p>-- or --</p>
+
+          </form>
+
+          <button className="--btn --btn-danger --btn-block">
+            <FaGoogle color="#fff"/>
+            Login With Google</button>
+
+            <span className={styles.register}>
+              <p>Don't have an account?</p>
+              <Link to="/register">Register</Link>
+            </span>
+      </div>
+      </Card>
+    </section>
+  ) 
 };
 
 export default login;
