ecaa8eb12d68fb1b2978d308116f6f83b61a2b9e
JOEX <oyinjoe23@gmail.com>
2022-12-20 00:19:46 +0100

    feat: Login

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,7 +1,35 @@
 import React from "react";
-
+import styles from "./auth.module.scss";
+import RegisterImg from "../../assets/register.png";
+import { Link } from "react-router-dom";
+import Card from "../../components/card/Card";
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <section className={` container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <h2>Register</h2>
+          <form>
+            <input type="email" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+            <input type="password" placeholder="Confirm Password" required />
+
+            <button className="--btn --btn-primary --btn-block">
+              Register
+            </button>
+          </form>
+
+          <span className={styles.register}>
+            <p>Already have an account?</p>
+            <Link to="/login">Login</Link>
+          </span>
+        </div>
+      </Card>
+      <div className={styles.img}>
+        <img src={RegisterImg} alt="Register" width="400" />
+      </div>
+    </section>
+  );
 };
 
 export default Register;
