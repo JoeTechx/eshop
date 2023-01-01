9686b440f2401f791841ccfb6524c100529dc49f
JOEX <oyinjoe23@gmail.com>
2023-01-01 23:47:53 +0100

    fix: formated some files

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,16 +1,14 @@
-import React from 'react'
-import styles from "./Footer.module.scss"
+import React from "react";
+import styles from "./Footer.module.scss";
 
 const date = new Date();
 const Year = date.getFullYear();
 const Footer = () => {
   return (
     <footer>
-      <div className={styles.footer}>
-        &copy; {Year} All Right Reserved.
-      </div>
+      <div className={styles.footer}>&copy; {Year} All Right Reserved.</div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
-import { getFirestore } from "firebase/firestore"
-import { getStorage } from "firebase/storage"
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyDh1CJTuFe7FLRWFZPhEZQD5k1s8HqLFcE",
@@ -9,7 +9,7 @@ export const firebaseConfig = {
   projectId: "eshop-6fe04",
   storageBucket: "eshop-6fe04.appspot.com",
   messagingSenderId: "936669559655",
-  appId: "1:936669559655:web:bc4f404179811b0606cdc2"
+  appId: "1:936669559655:web:bc4f404179811b0606cdc2",
 };
 
 // Initialize Firebase
@@ -18,4 +18,4 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
-export default app
+export default app;
